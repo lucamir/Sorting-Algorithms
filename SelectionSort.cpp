de4a0ed42456bf8063e1caee6396bfd6ef1115cd
Mirabella Luca Timoteo <lucamir@hotmail.it>
2017-09-13 20:07:11 +0200

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-template <class H> class bubbleSort{
+template <class H> class selectionSort{
   H array[10];
   int dim,start,end;
   H addElement(int x);
   void swap(int i,int j);
 public:
-  bubbleSort(){
+  selectionSort(){
     dim=10;
     start=0;
     end=dim-1;
@@ -19,7 +19,7 @@ public:
   void stamp();
 };
 
-template <class H> void bubbleSort<H>::sort(){
+template <class H> void selectionSort<H>::sort(){
   int min;
   for(int i=0;i<end;i++){
     min=i;
@@ -29,20 +29,20 @@ template <class H> void bubbleSort<H>::sort(){
   }
 }
 
-template <class H> void bubbleSort<H>::stamp(){
+template <class H> void selectionSort<H>::stamp(){
   cout<<endl;
   for(int i=0;i<dim;i++)
     cout<<array[i]<<" ";
   cout<<endl<<endl;
 }
 
-template <class H> void bubbleSort<H>::swap(int i,int j){
+template <class H> void selectionSort<H>::swap(int i,int j){
   int tmp=array[i];
   array[i]=array[j];
   array[j]=tmp;
 }
 
-template <class H> H bubbleSort<H>::addElement(int x){
+template <class H> H selectionSort<H>::addElement(int x){
   H temp;
   cout<<"Add a new element n."<<x+1<<" @> ";
   cin>>temp;
@@ -50,8 +50,9 @@ template <class H> H bubbleSort<H>::addElement(int x){
 }
 
 int main(){
-  bubbleSort<int> *x=new bubbleSort<int>();
+  selectionSort<int> *x=new selectionSort<int>();
   x->stamp();
   x->sort();
   x->stamp();
 }
+
