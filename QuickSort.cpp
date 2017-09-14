fd157b8b140e1b6d22ec1237715061a07cbd236b
Mirabella Luca Timoteo <lucamir@hotmail.it>
2017-09-14 20:47:59 +0200

    add a variable
    add a variable in the constructor for array dimensions. main deleted

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 
 template <class H> class quickSort{
-  H array[10];
+  H *array;
   int dim,start,end;
   H addElement(int x);
   void swap(int i,int j);
 public:
-  quickSort(){
-    dim=10;
+  quickSort(int dim){
+    this.dim=dim;
     start=0;
-    end=dim-1;
-    for(int i=0; i<dim; i++)
+    end=this.dim-1;
+    array=new H();
+    for(int i=0; i<this.dim; i++)
       array[i]=addElement(i);
 
   }
@@ -60,11 +61,4 @@ template <class H> H quickSort<H>::addElement(int x){
   cout<<"Add a new element n."<<x+1<<" @> ";
   cin>>temp;
   return  temp;;
-}
-
-int main(){
-  quickSort<int> *x=new quickSort<int>();
-  x->stamp();
-  x->sort();
-  x->stamp();
 }
