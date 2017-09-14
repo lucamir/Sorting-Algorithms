0ffdd019ab80166b5faad11752adb6e1980a4ce7
Mirabella Luca Timoteo <lucamir@hotmail.it>
2017-09-14 20:44:31 +0200

    add a variable
     add a variable in the constructor for array dimensions

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 
 template <class H> class bubbleSort{
-  H array[10];
+  H *array;
   int dim,start,end;
   H addElement(int x);
   void swap(int i,int j);
 public:
-  bubbleSort(){
-    dim=10;
+  bubbleSort(int dim){
+    this.dim=dim;
     start=0;
     end=dim-1;
-    for(int i=0; i<dim; i++)
+    array=new H(); 
+    for(int i=0; i<this.dim; i++)
       array[i]=addElement(i);
 
   }
@@ -46,11 +47,4 @@ template <class H> H bubbleSort<H>::addElement(int x){
   cout<<"Add a new element n."<<x+1<<" @> ";
   cin>>temp;
   return  temp;;
-}
-
-int main(){
-  bubbleSort<int> *x=new bubbleSort<int>();
-  x->stamp();
-  x->sort();
-  x->stamp();
 }
